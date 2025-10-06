6e926e2b1fe675303b509b380d362f12d185bf5d
Jamie Rogers <jrog0022@student.monash.edu>
2025-10-06 14:43:08 +1100

    typos and data path

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,6 +1,6 @@
 from app.user import User
 
 class Admin(User):
-    """Represents a patient, inheriting from the base User class."""
+    """Represents an admin, inheriting from the base User class."""
     def __init__(self, user_id, name):
         super().__init__(user_id, name)
